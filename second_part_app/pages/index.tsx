a092fd9ff1b77eaa7da7c4df0d0605221301fb77
Kirill Petunin <kpetunin@aligntech.com>
2021-08-14 13:15:20 +0400

    Starts using svg (don't work)

--- second_part_app/pages/index.tsx
+++ second_part_app/pages/index.tsx
@@ -6,7 +6,9 @@ export default function Home(): JSX.Element {
     <>
       <Htag tag="h1">Text</Htag>
       <Button appearance="primary">Button</Button>
-      <Button appearance="ghost">Button</Button>
+      <Button appearance="ghost" arrow="right">
+        Button
+      </Button>
     </>
   )
 }
