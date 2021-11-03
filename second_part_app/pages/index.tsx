5efe094cd62b823f683da9f58e9599665cc9b296
Kirill Zhukovskiy <zhuwork@yandex.ru>
2021-11-03 20:43:59 +0300

    [61] - using GetStaticProps

--- second_part_app/pages/index.tsx
+++ second_part_app/pages/index.tsx
@@ -1,43 +1,37 @@
-import React, { useState } from 'react'
-import { Button, Htag, P, Tag } from '../components'
-import { Rating } from '../components/Rating'
+import axios from 'axios'
+import { GetStaticProps } from 'next'
+import React from 'react'
+import { MenuItem } from '../interfaces/menu.interface'
 import { withLayout } from '../layout/Layout'
 
-function Home(): JSX.Element {
-	const [rating, setRating] = useState<number>(4)
+const Home = ({ menu }: HomeProps): JSX.Element => {
 	return (
 		<>
-			<Htag tag='h1'>Headlines</Htag>
-			<Htag tag='h1'>H1</Htag>
-			<Htag tag='h2'>H2</Htag>
-			<Htag tag='h3'>H3</Htag>
-			<hr />
-			<Htag tag='h1'>Buttons</Htag>
-			<Button appearance='primary' arrow='down'>
-				Button
-			</Button>
-			<Button appearance='ghost' arrow='right'>
-				Button
-			</Button>
-			<hr />
-			<Htag tag='h1'>Paragraphs</Htag>
-			<P size='L'>I'm a large-sized version of the P</P>
-			<P>I'm a medium-sized version of the P</P>
-			<P size='S'>I'm a small-sized version of the P</P>
-			<hr />
-			<Htag tag='h1'>Tags</Htag>
-			<Tag size='L'>Large</Tag>
-			<Tag>Medium</Tag>
-			<Tag color='ghost'>ghost</Tag>
-			<Tag color='red'>red</Tag>
-			<Tag color='gray'>gray</Tag>
-			<Tag color='green'>green</Tag>
-			<Tag color='primary'>primary</Tag>
-			<hr />
-			<Htag tag='h1'>Rating</Htag>
-			<Rating rating={rating} setRating={setRating} isEditable />
+			<ul>
+				{menu.map(item => (
+					<li key={item._id.secondCategory}>{item._id.secondCategory}</li>
+				))}
+			</ul>
 		</>
 	)
 }
 
 export default withLayout(Home)
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory,
+	})
+	return {
+		props: {
+			menu,
+			firstCategory,
+		},
+	}
+}
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[]
+	firstCategory: number
+}
